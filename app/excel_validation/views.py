beb32a2a42c21acefc9a9f6ca76409fb704a9626
Гришин Кирилл <grishi.k@yandex.ru>
2021-10-23 17:50:44 +0300

    поправил month и убрал () в ручке

--- app/excel_validation/views.py
+++ app/excel_validation/views.py
@@ -1,6 +1,5 @@
-import pyexcel
-from pyexcel._compact import OrderedDict
-from openpyxl import workbook, load_workbook
+from datetime import datetime, timedelta
+from openpyxl import load_workbook
 from flask import Blueprint, request
 
 excel_validation_bp = Blueprint('validation', __name__, url_prefix='/validation')
@@ -14,21 +13,22 @@ def making_dict():
     file = request.files['file']
     wb = load_workbook(file)
     sheet = wb.get_sheet_by_name('График ЗП')
-    mounth = sheet.cell(row=4, column=2).value
+    month = sheet.cell(row=4, column=2).value
 
     data = sheet.iter_rows(min_row=4, max_row=4, min_col=5, max_col=35, values_only=True)
     hours = sheet.iter_rows(min_row=5, max_row=5, min_col=5,  max_col=35, values_only=True)
     dictionary = dict(zip(*data, *hours))
-    print(mounth)
-    print(dictionary)
+    Float_dictinary = str(dictionary)
+    print(month)
+    print(Float_dictinary)
     return {"ok": True}
 
-@excel_validation_bp.route("/get_file(check)", methods=["POST"])
+@excel_validation_bp.route("/get_file_check", methods=["POST"])
 def load_def_fail():
     file = request.files['file2']
     wb = load_workbook(file)
     sheet = wb.active
-    mounth = sheet.cell(row=10, column=33).value
+    month = sheet.cell(row=10, column=33).value
     surname = sheet.cell(row=21, column=2).value
 
     data_1 = sheet.iter_rows(min_row=14, max_row=14, min_col=4, max_col=18, values_only=True)
@@ -38,7 +38,9 @@ def load_def_fail():
     dictionary = dict(zip(*data_1, *hours_1))
     dictionary_2 = dict(zip(*data_2, *hours_2))
     dictionary.update(dictionary_2)
-    print(mounth)
+
+
+    print(month)
     print(surname)
     print(dictionary)
     return {"ok": True}
